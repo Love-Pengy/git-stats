cf6ed558ced3265317e0f9d0f9005ad3ab83ee6e
Love-Pengy <afrazierboy2001@gmail.com>
2025-03-18 17:52:01 -0400

    fix build script, add compile commands generation

--- dev/buildProject.sh
+++ dev/buildProject.sh
@@ -1,13 +1,17 @@
 #!/bin/sh
 
 cd ..
+
 # Remove installed .deb packages
 sudo apt-get purge git-stats
-rm -rf ./release/ 
-rm -rf ~/.config/obs-studio/plugins/git-stats/
-.github/scripts/build-linux #--skip-deps 
-mkdir -p ./release/RelWithDebInfo/share/obs/obs-plugins/git-stats/bin/64bit
-cp ./release/RelWithDebInfo/lib/x86_64-linux-gnu/obs-plugins/git-stats.so ./release/RelWithDebInfo/share/obs/obs-plugins/git-stats/bin/64bit
-mkdir ./release/RelWithDebInfo/share/obs/obs-plugins/git-stats/data
-mv ./release/RelWithDebInfo/share/obs/obs-plugins/git-stats/locale  ./release/RelWithDebInfo/share/obs/obs-plugins/git-stats/data/
-mv ./release/RelWithDebInfo/share/obs/obs-plugins/git-stats ~/.config/obs-studio/plugins
+cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=1 --preset ubuntu-x86_64 --fresh 
+cmake --build --preset ubuntu-x86_64
+mkdir -p build_x86_64/git-stats
+mkdir -p build_x86_64/git-stats/bin
+mkdir -p build_x86_64/git-stats/data
+mkdir -p build_x86_64/git-stats/bin/64bit
+mkdir -p build_x86_64/git-stats/data/locale
+cp build_x86_64/git-stats.so build_x86_64/git-stats/bin/64bit 
+# mv ./release/RelWithDebInfo/share/obs/obs-plugins/git-stats/locale  ./release/RelWithDebInfo/share/obs/obs-plugins/git-stats/data/
+rm -rf ~/.config/obs-studio/plugins/git-stats
+mv build_x86_64/git-stats ~/.config/obs-studio/plugins
